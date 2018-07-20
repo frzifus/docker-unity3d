744dabab54b7e0a11fce9a389890c1b831d83917
frzifus <benne@klimlive.de>
2018-07-20 02:54:27 +0200

    Fix: “Failed to initialize unity graphics” error

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 if [ ! -f /opt/Unity-2018.2.0f2/Editor/Unity ]; then
     /home/user/UnitySetup
 else 
-    /opt/Unity-2018.2.0f2/Editor/Unity -force-glcore
+    # https://www.davidezordan.net/blog/?p=8171
+    /opt/Unity-2018.2.0f2/Editor/Unity -force-driver-type-warp
 fi
