2210b1c9c1bb4aa370c95cfcb8308cfba11797dd
frzifus <benne@klimlive.de>
2018-07-20 02:42:53 +0200

    force opengl

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 if [ ! -f /opt/Unity-2018.2.0f2/Editor/Unity ]; then
     /home/user/UnitySetup
 else 
-    /opt/Unity-2018.2.0f2/Editor/Unity
+    /opt/Unity-2018.2.0f2/Editor/Unity -force-glcore
 fi
